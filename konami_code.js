e9fbe8a79f201cf18d783cda12876fc2b3ed79d0
Katherine Gruenberg <katherine.gruenberg@gmail.com>
2018-05-21 05:50:57 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -2,5 +2,20 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // Write your JavaScript code inside the init() function
+let i = 0;
 
+document.body.addEventListner('keydown', function(event){
+  const key = parseInt(event.detail || event.which);
+
+  if (key == code[i]) {
+    i++;
+
+    if(i === code.length) {
+      alert("Congratulations, nerd!");
+    }
+  } else {
+    i = 0;
+    
+  }
 }
+};
